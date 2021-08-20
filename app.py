96c9ddcf6acdaa6c5c6c8273d57246c02217fed4
nokyung-009 <89238935+nokyung-009@users.noreply.github.com>
2021-08-20 12:17:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 data = [
         {
@@ -23,11 +23,19 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello Flask-Heroku"
 
 @app.route('/api', methods=['GET'])
 def get_api():
     return jsonify(data)
 
-if __name__ == "__main__":
+@app.route('/hello/<string:name>')
+def Home(name):
+	return render_template('home.html', name_html=name)
+
+@app.route('/name')
+def name():
+    return "<font color=Green>จุฑารัตน์</font> <font color=pink>อยู่เจียม</font> <br> <font color=blue>เลขที่9 ม.4/10</font> "
+
+if _name_ == "_main_":
     app.run(debug=False)
